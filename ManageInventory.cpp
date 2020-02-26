eac19f3d07ef8b6eccb2eab1af217dbd3dd15c6c
Krys <21113012+Krystade@users.noreply.github.com>
2020-02-25 17:23:59 -0800

    Update ManageInventory.cpp

--- ManageInventory.cpp
+++ ManageInventory.cpp
@@ -24,26 +24,20 @@ ManageInventory::ManageInventory(const ManageInventory& source){
 	p_pInventoryItems = new Item*[size];
 	//Loop through each value in the original's array in order to make a deep copy
 	for(int i = 0; i < source.count; i++){
-		p_pInventoryItems[i] = new Item();
-		p_pInventoryItems[i]->name = source.p_pInventoryItems[i]->name;
-		p_pInventoryItems[i]->quantity = source.p_pInventoryItems[i]->quantity;
-		p_pInventoryItems[i]->cost = source.p_pInventoryItems[i]->cost;
+		p_pInventoryItems[i] = new Item{name, quantity, cost};
 		count++;
 	}
 }
 //ManageInventory destructor
 ManageInventory::~ManageInventory(){
-	for(int i = 0; i < size; i++){
+	for(int i = 0; i < count; i++){
 		delete p_pInventoryItems[i];
 	}
 	delete[] p_pInventoryItems;
 }
 //Function to add a new Item into the p_pInventoryItems array
 void ManageInventory::addItem(string name, int quantity, float cost){
-	p_pInventoryItems[count] = new Item();
-	p_pInventoryItems[count]->name = name;
-	p_pInventoryItems[count]->quantity = quantity;
-	p_pInventoryItems[count]->cost = cost;
+	p_pInventoryItems[count] = new Item{name, quantity, cost};
 	count++;
 }
 //Print out the values of a ManageInventory object
